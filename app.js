95c06c8c5b4388c588f6b4022f049e690d7f1959
aditya06121 <adityaraj7320@gmail.com>
2026-03-13 13:00:55 +0530

    code optimized for wsl

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 // Allow larger JSON payloads so `/api/seed` can accept large multiline SQL
 app.use(express.json({ limit: "5mb" }));
 
+
 import submitRouter from "./routes/submit.routes.js";
 app.use("/api", submitRouter);
 
